9dd91c6a162fdc4a43536404f71a0783bbbd046d
gundorovnikita <gundorovnikita@yahoo.com>
2019-09-14 19:33:00 +0400

    first commit

---
+++ src/views/Todos.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h2>Todo applicaton</h2>
+    <router-link to="/">home</router-link>
+    <hr>
+    <AddTodo
+      @add-todo="addTodo"
+    />
+    <hr>
+    <Loader v-if="loading" />
+    <TodoList
+     v-else-if="todos.length"
+     v-bind:todos='todos'
+     @remove-todo='removeTodo'
+    />
+    <p v-else>No todos</p>
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+import Loader from '@/components/Loader'
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      todos:[],
+      loading:true
+    }
+  },
+  mounted(){
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=4')
+    .then(response => response.json())
+    .then(json => {
+      setTimeout(()=>{
+        this.todos=json
+        this.loading=false
+      },1000)
+      
+    })
+  },
+  methods:{
+    removeTodo(id){
+      this.todos = this.todos.filter(t=>t.id !== id)
+    },
+    addTodo(todo){
+      this.todos.push(todo)
+    }
+  },
+  components: {
+    TodoList, AddTodo, Loader
+  },
+  
+}
+</script>
